c47e55993eac082f6d87f394fb05010d06ae05ee
Angel Colman <colmansaggia@gmail.com>
2020-11-12 20:08:36 -0300

    cambios varios

--- controlador.py
+++ controlador.py
@@ -5,8 +5,9 @@ from flask_wtf import CSRFProtect
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
-from form import FormCambioPassword
+from form import FormCambioPassword,FormRegistroPersona
 import datetime
+import base64
 
 
 
@@ -27,6 +28,7 @@ Carreras = Base.classes.carreras
 Materias = Base.classes.materias
 CarrerasMaterias = Base.classes.carrera_materias
 TitularMaterias = Base.classes.titular_materias
+AlumnoMaterias = Base.classes.alumno_materias
 
 
 fecha = datetime.datetime.now()
@@ -97,18 +99,21 @@ def cambioPassword():
 @app.route('/procesar_cambio_password',methods=['POST'])
 def procesarCambioPassword():
     mensajes = []
-    form = FormCambioPassword(request.form)
-    if request.method == 'POST' and form.validate_on_submit():
-        datos = request.form
-        passActual = datos['txtPasswordActual']
-        passNueva  = datos['txtPasswordNueva']
-        comprobar = cambiarPassword(passActual,passNueva)
-        if comprobar==True:
-           mensajes = "correcto"
-        else:
-           mensajes = "incorrecto"
-    for error in form.errors.items():
-        mensajes.append(error)
+    if session.get('conectado') == 'S':
+        form = FormCambioPassword(request.form)
+        if request.method == 'POST' and form.validate_on_submit():
+            datos = request.form
+            passActual = datos['txtPasswordActual']
+            passNueva  = datos['txtPasswordNueva']
+            comprobar = cambiarPassword(passActual,passNueva)
+            if comprobar==True:
+                mensajes = "correcto"
+            else:
+                mensajes = "incorrecto"
+        for error in form.errors.items():
+            mensajes.append(error)
+    else:
+        mensajes = "incorrecto"
     return jsonify(mensajes)
 
 
@@ -127,20 +132,20 @@ def cambiarPassword(passActual,passNueva):
     else:
         return False
 
-@app.route('/crear_usuario')
-def crearUsuario():
-    titulo = "COLFEAR | Crear Usuario"
-    return render_template('crearUsuario.html',titulo=titulo)
-
 
 @app.route('/registrar_persona')
 def registrarPersona():
-    titulo = "COLFEAR | Registrar Personas"
-    carreras = db.session.query(Carreras).all()
-    return render_template('registrarPersona.html',titulo=titulo,carreras=carreras)
+    if session.get('conectado')=='S':
+        titulo = "COLFEAR | Registrar Personas"
+        carreras = db.session.query(Carreras).all()
+        return render_template('registrarPersona.html',titulo=titulo,carreras=carreras)
+    else:
+        flash(u'Debe ingresar sus credenciales para ingresar al sistema', 'warning')
+        return redirect(url_for('index'))
 
 @app.route('/obtener_materias_alumnos',methods=['POST'])
 def obtenerMateriasAlumnos():
+    resultado = None
     if request.method=='POST':
         datos = request.form
         carrera = datos['carrera']
@@ -155,6 +160,7 @@ def obtenerMateriasAlumnos():
 
 @app.route('/obtener_titular_materias',methods=['POST'])
 def obtenerTitularMaterias():
+    resultado = None
     if request.method=='POST':
         datos = request.form
         materia = datos['materia']
@@ -167,6 +173,107 @@ def obtenerTitularMaterias():
             i = i + 1
 
     return resultado
+
+@app.route('/registrar_persona',methods=['POST'])
+def procesarRegistroPersona():
+    mensajes = []
+    form = FormRegistroPersona(request.form)
+    if request.method=='POST' and form.validate_on_submit():
+        datos = request.form
+        primerNombre = datos['txtPrimerNombre']
+        segundoNombre = datos['txtSegundoNombre']
+        primerApellido = datos['txtPrimerApellido']
+        segundoApellido = datos['txtSegundoApellido']
+        nroCedula = datos['txtNroCedula']
+        sexo = datos['sltSexo']
+        fechaNacimiento = datos['txtFechaNacimiento']
+        if fechaNacimiento!="":
+            fechaNacimiento = datetime.datetime.strptime(datos['txtFechaNacimiento'], '%Y-%m-%d').strftime('%d/%m/%Y')
+        else:
+            fechaNacimiento = None
+        estadoCivil = datos['sltEstadoCivil']
+        alumno = request.form.get('chkAlumno')
+        profesor = request.form.get('chkProfesor')
+        titular = request.form.getlist('txtTitular')
+        materiasProfesor = request.form.getlist('txtMateriaProfesor[]')
+        turnosProfesor = request.form.getlist('txtTurnoProfesor[]')
+        gruposProfesor = request.form.getlist('txtGrupoProfesor[]')
+        modosProfesor = request.form.getlist('txtModoProfesor[]')
+        horaInicio = request.form.getlist('txtHoraInicioProfesor[]')
+        horaFin = request.form.getlist('txtHoraFinProfesor[]')
+        cantidad = db.session.query(Personas).filter(Personas.pers_prim_nombre==primerNombre.upper(),Personas.pers_seg_nombre==segundoNombre.upper(),Personas.pers_prim_apellido==primerApellido.upper(),Personas.pers_seg_apellido==segundoApellido.upper(),Personas.pers_nro_doc==nroCedula,Personas.pers_sexo==sexo,Personas.pers_fecha_nacimiento==fechaNacimiento,Personas.pers_estado_civil==estadoCivil).count()
+        if cantidad==0:
+            try:
+                db.session.add(Personas(
+                    pers_prim_nombre = primerNombre.upper(),
+                    pers_seg_nombre = segundoNombre.upper(),
+                    pers_prim_apellido = primerApellido.upper(),
+                    pers_seg_apellido = segundoApellido.upper(),
+                    pers_nro_doc = nroCedula,
+                    pers_sexo = sexo,
+                    pers_fecha_nacimiento = fechaNacimiento,
+                    pers_estado_civil = estadoCivil,
+                    pers_alumno = alumno,
+                    pers_profesor = profesor,
+                    pers_fecha_registro = fechaActual,
+                    pers_usua_cod_registro = session.get('usua_cod')
+                ))
+                db.session.flush()
+                ultimoCod = db.session.query(Personas).order_by(Personas.pers_cod.desc()).first()
+                cantAlumno = len(titular)
+                cantProfesor = len(materiasProfesor)
+                if alumno!=None:
+                   recorrido = 0
+                   while recorrido<cantAlumno:
+                     db.session.add(AlumnoMaterias(
+                        alma_pers_cod = ultimoCod.pers_cod,
+                        alma_tima_cod = titular[recorrido],))
+                     recorrido +=1
+                     db.session.flush()
+                if profesor!=None:
+                    recorrido = 0
+                    while recorrido<cantProfesor:
+                        db.session.add(TitularMaterias(
+                            tima_pers_cod = ultimoCod.pers_cod,
+                            tima_came_cod = materiasProfesor[recorrido],
+                            tima_turno = turnosProfesor[recorrido],
+                            tima_grupo = gruposProfesor[recorrido],
+                            tima_modalidad = modosProfesor[recorrido],
+                            tima_horario_inicio = horaInicio[recorrido],
+                            tima_horario_culminacion = horaFin[recorrido],
+                            tima_anio = fecha.strftime("%Y")
+                        ))
+                        recorrido +=1
+                        db.session.flush()
+                db.session.commit()
+                mensajes = "correcto"
+            except:
+                db.session.rollback()
+                mensajes = "incorrecto"
+        else:
+            mensajes = "existe"
+    for error in form.errors.items():
+        mensajes.append(error)
+    return jsonify(mensajes)
+
+@app.route('/listado_personas')
+def listadoPersonas():
+    if session.get('conectado')=='S':
+        titulo = "COLFEAR | Listado Personas"
+        personas = db.session.query(Personas).all()
+        return render_template('listadoPersonas.html',titulo=titulo,personas=personas)
+    else:
+        flash(u'Debe ingresar sus credenciales para ingresar al sistema', 'warning')
+        return redirect(url_for('index'))
+
+@app.route('/modificar_datos_personas/<cod>')
+def modificarDatosPersonas(cod):
+    persCod = base64.b64decode(cod)
+    carreras = db.session.query(Carreras).all()
+    personas = db.session.query(Personas).filter(Personas.pers_cod==persCod).first()
+    alumno = db.session.query(AlumnoMaterias).filter(AlumnoMaterias.alma_pers_cod==persCod).all()
+    profesor = db.session.query(TitularMaterias).filter(TitularMaterias.tima_pers_cod==persCod).all()
+    return render_template('modificarPersona.html', carreras=carreras,personas=personas,alumno=alumno,profesor=profesor)
 
 @app.route('/cerrar_sesion')
 def cerrarSesion():
--- form.py
+++ form.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField
+from wtforms import PasswordField,StringField,IntegerField,SelectField,SelectField,BooleanField
 from wtforms.validators import DataRequired,EqualTo,Length
 
 class FormCambioPassword(FlaskForm):
     txtPasswordActual = PasswordField('txtPasswordActual',validators=[DataRequired("Contraseña Actual es un campo obligatorio")])
     txtPasswordNueva  = PasswordField('txtPasswordNueva',validators=[DataRequired("Contraseña Nueva es un campo obligatorio"),EqualTo('txtPasswordConfirmacion',message="La contraseña nueva y la confirmación no coinciden"),Length(min=8,message="La longitud mínima debe ser de %(min)s carácteres")])
-    txtPasswordConfirmacion = PasswordField('txtPasswordConfirmacion') 
+    txtPasswordConfirmacion = PasswordField('txtPasswordConfirmacion')
+
+class FormRegistroPersona(FlaskForm):
+    txtPrimerNombre = StringField('txtPrimerNombre',validators=[DataRequired("Primer Nombre es un campo obligatorio"),Length(max=30,message="La longitud máxima del campo Primer Nombre debe ser de %(max)s carácteres")])
+    txtSegundoNombre = StringField('txtSegundoNombre',validators=[Length(max=30,message="La longitud máxima del campo Segundo Nombre debe ser de %(max)s carácteres")])
+    txtPrimerApellido = StringField('txtPrimerApellido',validators=[DataRequired("Primer Nombre es un campo obligatorio"),Length(max=50,message="La longitud máxima del campo Primer Apellido debe ser de %(max)s carácteres")])
+    txtSegundoApellido = StringField('txtSegundoApellido',validators=[Length(max=30,message="La longitud máxima del campo Segundo Apellido debe ser de %(max)s carácteres")])
+    txtNroCedula = IntegerField('txtNroCedula',validators=[DataRequired("Nro. de Cédula es un campo obligatorio")])
+    txtFechaNacimiento = StringField('txtFechaNacimiento')
